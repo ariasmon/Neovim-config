f6e71c13670317bfc4bdb70afabaef00e9bfdf3e
Arias <pablo.arias.mon@gmail.com>
2026-03-20 13:49:10 +0100

    Spell check quitado

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -30,7 +30,6 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = { "markdown", "text" },
     callback = function()
         vim.opt_local.wrap = true
-        vim.opt_local.spell = true
     end,
 })
 
